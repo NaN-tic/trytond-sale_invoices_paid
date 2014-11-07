51b490bb66573d3f352403db4edbc543c2a83328
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:38:25 +0100

    Cleanup tests

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-#This file is part sale_invoices_paid module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part sale_invoices_paid module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from setuptools import setup
 import re
