39557e863942344d402501585b97cc2fb47e7ab3
C?dric Krier <ced@b2ck.com>
2018-02-14 15:13:37 +0100

    Update to 4.7

--- __init__.py
+++ __init__.py
@@ -2,9 +2,10 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-from .sale import *
+from . import sale
+
 
 def register():
     Pool.register(
-        Sale,
+        sale.Sale,
         module='sale_invoices_paid', type_='model')
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,9 @@
 # This file is part sale_invoices_paid module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from .test_sale_invoices_paid import suite
+try:
+    from trytond.modules.sale_invoices_paid.tests.test_sale_invoices_paid import suite
+except ImportError:
+    from .test_sale_invoices_paid import suite
 
 __all__ = ['suite']
